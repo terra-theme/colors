e4b2e12efd8c1b92ce0e87511e2dde8dcbbee9f6
nbr <nikolaus.brunner@protonmail.ch>
2025-02-17 18:10:25 +0100

    refactor(themes): Split black-atom-engineering theme into modular components
    * Extract `syntax` and `ui` into separate files for better code organization
    * Move theme into `default` subdirectory to match new structure

---
+++ src/themes/default/black-atom-engineering.ts
@@ -0,0 +1,66 @@
+import * as Theme from "../../types/theme";
+
+import syntax_dark from "./syntax_dark";
+import ui_dark from "./ui_dark";
+
+const meta: Theme.Meta = {
+  key: "black-atom-engineering",
+  label: "Black Atom - Default [ENG]",
+  appearance: "dark",
+  status: "release",
+  collection: {
+    key: "default",
+    label: "Default"
+  }
+};
+
+const primaries: Theme.Primaries = [
+  "#040505",
+  "#0e1110",
+  "#181b19",
+  "#1d201e",
+  "#3e4441",
+  "#565f59",
+  "#606a65",
+  "#79857f",
+  "#9fafa7",
+  "#abbbb3",
+  "#c4d4cc",
+  "#d0e0d8"
+];
+
+const palette: Theme.Palette = {
+  black: primaries[2],
+  gray: primaries[5],
+
+  darkRed: "#9891d6",
+  red: "#a6a1df",
+
+  darkGreen: "#93c270",
+  green: "#a0ce7d",
+
+  darkYellow: primaries[9],
+  yellow: "#9BE197",
+
+  darkBlue: primaries[8],
+  blue: primaries[10],
+
+  darkMagenta: "#a498e9",
+  magenta: "#b7aded",
+
+  darkCyan: "#6ac779",
+  cyan: "#88dc96",
+
+  lightGray: primaries[8],
+  white: primaries[11]
+};
+
+const theme: Theme.Definition = {
+  meta,
+  primaries,
+  palette,
+  ui: ui_dark(primaries, palette),
+  syntax: syntax_dark(primaries, palette)
+};
+
+export default theme;
