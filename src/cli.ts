4181fe4259c32b61b03f343f161ecd9bd9c113df
nbr <nikolaus.brunner@protonmail.ch>
2025-02-18 15:34:10 +0100

    feat(cli): find and parse adapter config file
    * Add `@std/yaml` dependency to `import_map.json`
    * Implement `getAdapterConfig` function to load and parse `adapter.yml`
    * Add error handling for missing adapter configuration

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,28 @@
-import { config } from "./config.ts";
+import { parse as parseYml } from "@std/yaml";
+
+async function getAdapterConfig() {
+    try {
+        const adapterConfig = await Deno.readTextFile("adapter.yml");
+        const config = parseYml(adapterConfig);
+        return config;
+    } catch (error) {
+        if (error instanceof Deno.errors.NotFound) {
+            console.error("Error: adapter.yaml not found in current directory");
+            Deno.exit(1);
+        }
+        throw error;
+    }
+}
 
 if (import.meta.main) {
     const command = Deno.args[0];
-    const cwd = Deno.cwd();
-    console.log("DEBUG(cli.ts): cwd", cwd);
-    const themesPath = config.dir.themes;
-    console.log("DEBUG(cli.ts): themesPath", themesPath);
 
     switch (command) {
-        case "generate":
-            console.log("TODO: generate");
+        case "generate": {
+            const adapterConfig = await getAdapterConfig();
+            console.log("DEBUG(cli.ts): adapterConfig", adapterConfig);
             break;
+        }
 
         default:
             console.log(`
